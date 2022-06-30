50062e6a66cf745dfb732be5f67e052844fb2313
user <tre@Brian.local>
2022-06-30 23:16:30 +0300

    seeded data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,27 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+    
+    get "/posts" do
+      posts = Post.all.order(:created_at)
+      posts.to_json
+    end
+    
+    post "/posts" do
+      post = Post.create(content: params[:content], title: params[:title], category: params[:category], username: params[:username])
+      post.to_json
+    end
+    
+    patch "/posts/:id" do
+      post = Post.find(params[:id])
+      post.update(content: params[:content])
+      post.to_json
+    end
+    
+    delete "/posts/:id" do
+      post = Post.find(params[:id])
+      post.destroy
+      post.to_json
+    end
   end
-
-end
+  
